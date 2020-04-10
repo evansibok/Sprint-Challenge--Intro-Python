facfdf65be27e81d52a8c449d54da77017a687ae
Evans Ibok <evansibok@gmail.com>
2020-04-10 09:59:24 +0100

    Add Vehicle class, FlightVehicle class and FlightVehicle subclasses

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,35 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+
+class Vehicle:  # Vehicle class
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        print(f"{self.name}")
+
+
+class FlightVehicle(Vehicle):  # FlightVehicle as a subclass of Vehicle
+    def __init__(self, name):
+        super().__init__(name)
+        pass
+
+
+class Starship(FlightVehicle):  # Starship as a subclass of FlightVehicle
+    def __init__(self, name):
+        super().__init__(name)
+        pass
+
+
+class Airplane(FlightVehicle):  # Airplane as a subclass of FlightVehicle
+    def __init__(self, name):
+        super().__init__(name)
+        pass
+
+
+class GroundVehicle(Vehicle):  # GroundVehicle as a subclass of Vehicle
+    def __init__(self, name):
+        super().__init__(name)
+        pass
